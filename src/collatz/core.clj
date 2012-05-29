3e33ae215ecbe068ce3111529d546b1ee0af7902
David Greenberg <dsg123456789@gmail.com>
2012-05-29 10:05:18 -0400

    initial commit

---
+++ src/collatz/core.clj
@@ -0,0 +1,22 @@
+(ns collatz.core)
+
+(defn hail-step [^long n]
+  (if (even? n)
+    (bit-shift-right n 1)
+    (unchecked-inc (unchecked-multiply n 3))))
+
+(defn hailseq
+  [seed]
+  (take-while pos? (iterate hail-step seed)))
+
+(defn maxhailseq
+  "Maximize the hailstone seq over the given range"
+  [over]
+  (reduce #(max (second %1) (second %2))
+          (pmap #(vector % (count (hailseq %)))
+               over)))
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
